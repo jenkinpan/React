cea7f6a1996af7f72b5a04ea51f409be94dc0779
jenkinpan <jenkinpan0628@gmail.com>
2023-12-13 17:45:16 +0800

    destructing data

--- 2.0 JS-review/script.js
+++ 2.0 JS-review/script.js
@@ -142,3 +142,9 @@ function getBooks() {
 function getBook(id) {
   return data.find((d) => d.id === id);
 }
+
+// destructing data
+const book = getBook(2);
+
+const { title, author } = book;
+console.log(title, author);
