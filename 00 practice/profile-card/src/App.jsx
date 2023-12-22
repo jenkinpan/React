a8c3a22c1c226742d2fafe3f566174eba6c50790
jenkinpan <jenkinpan0628@gmail.com>
2023-12-22 15:06:36 +0800

    Add profile card components and styles

--- 00 practice/profile-card/src/App.jsx
+++ 00 practice/profile-card/src/App.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import "./App.css";
 
 function App() {
@@ -16,13 +17,43 @@ function App() {
 }
 
 function Avatar() {
-  return <img src="" alt="" />;
+  return <img src="jenkin.jpg" alt="jenkin" className="avatar" />;
 }
 
 function Intro() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Jenkin Pan</h1>
+      <p>Web Developer</p>
+    </div>
+  );
 }
 
-function SkillList() {}
+function SkillList() {
+  return (
+    <ul className="skill-list">
+      <Skill name="JavaScript" level="💪" color="#123456" />
+      <Skill name="Python" level="👶" color="orangered" />
+      <Skill name="HTML+CSS" level="💪" color="yellow" />
+      <Skill name="React" level="💪" color="orangered" />
+      <Skill name="Vue" level="👶" color="lightblue" />
+    </ul>
+  );
+}
+
+Skill.propTypes = {
+  name: PropTypes.string.isRequired,
+  level: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+};
+
+function Skill(props) {
+  return (
+    <li className="skill" style={{ backgroundColor: props.color }}>
+      {props.name}
+      <span>{props.level}</span>
+    </li>
+  );
+}
 
 export default App;
