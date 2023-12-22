005046ed53bc381abe818aff728a3e29fd83a871
jenkinpan <jenkinpan0628@gmail.com>
2023-12-22 13:42:19 +0800

    using Vite recreate pizza-menu project

---
+++ pizza-menu/src/App.jsx
@@ -0,0 +1,116 @@
+import PropTypes from "prop-types";
+import "./App.css";
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+};
+
+const Menu = () => {
+  return (
+    <main className="Menu">
+      <h2>Our Menu</h2>
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        data={pizzaData}
+      />
+    </main>
+  );
+};
+
+Pizza.propTypes = {
+  photoName: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  ingredients: PropTypes.string.isRequired,
+};
+
+function Pizza(props) {
+  return (
+    <div>
+      <img src={props.photoName} alt={props.name} />
+      <h3>{props.name}</h3>
+      <p>{props.ingredients}</p>
+    </div>
+  );
+}
+
+const Footer = () => {
+  const hour = new Date().getHours();
+  const open = 12;
+  const closed = 22;
+  const isOpen = hour >= open && hour <= closed;
+
+  console.log(isOpen);
+
+  // * If the hour is less than the open time or greater than the closed time, then we're closed
+  // if (hour >= open && hour <= closed) alert("We're currently open!");
+  // else alert("We're currently closed!");
+
+  return (
+    <footer className="footer">
+      It is {new Date().toLocaleDateString()} now, We&apos;re currently open!
+    </footer>
+  );
+};
+
+export default App;
