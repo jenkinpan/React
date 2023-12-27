f42b18cc7c7b13d2db561bc5ec0d9e332057fdf6
jenkinpan <jenkinpan0628@gmail.com>
2023-12-27 22:13:42 +0800

    Add step navigation and buttons to App component

--- 4.0-steps/src/App.jsx
+++ 4.0-steps/src/App.jsx
@@ -1,5 +1,54 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 function App() {
-  return <div className="steps">Hello React!</div>;
+  const [step, setStep] = useState(1);
+
+  function handelPrevious() {
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  }
+
+  function handleNext() {
+    if (step < messages.length) {
+      setStep(step + 1);
+    }
+  }
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handelPrevious}
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
