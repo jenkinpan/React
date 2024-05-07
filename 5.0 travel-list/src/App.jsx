d44c19742ab88c584dd23a5ea6b014ebb22ad9f8
jenkin <jenkinpan0628@gmail.com>
2024-05-07 12:44:22 +0800

    delete app.css and add form into app component

--- 5.0 travel-list/src/App.jsx
+++ 5.0 travel-list/src/App.jsx
@@ -1,8 +1,12 @@
-import "./App.css";
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "chager", quantity: 1, packed: false },
+];
 
 function App() {
   return (
-    <main>
+    <main className="app">
       <Logo />
       <Form />
       <PackingList />
@@ -16,13 +20,55 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((i) => (
+          <option value={i} key={i}>
+            {i}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
+  );
+}
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
     </div>
   );
 }
-function PackingList() {}
-function Stats() {}
+
+function Item({ item }) {
+  return (
+    <li>
+      <input type="checkbox" />
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>👜You have X items on your list, and you already packed X (x%)</em>
+    </footer>
+  );
+}
 
 export default App;
