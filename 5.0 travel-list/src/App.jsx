2ac2a1dfffe233fd6115640085041ec1641592d4
jenkin <jenkinpan0628@gmail.com>
2024-05-07 12:48:30 +0800

    update nodejs and add proptype into Props in react

--- 5.0 travel-list/src/App.jsx
+++ 5.0 travel-list/src/App.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -50,6 +52,10 @@ function PackingList() {
     </div>
   );
 }
+
+Item.propTypes = {
+  item: PropTypes.object.isRequired,
+};
 
 function Item({ item }) {
   return (
